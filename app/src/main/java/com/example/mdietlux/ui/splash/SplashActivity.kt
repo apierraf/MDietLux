3f33e7d46300405d0d9ad3f3b822da6ca2437692
apierraf <apierrf88@gmail.com>
2021-01-16 02:09:05 -0500

    Login Done

---
+++ app/src/main/java/com/example/mdietlux/ui/splash/SplashActivity.kt
@@ -0,0 +1,64 @@
+package com.example.mdietlux.ui.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.lifecycle.ViewModelProvider
+import com.example.mdietlux.MainActivity
+import com.example.mdietlux.R
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var splashViewModel: SplashViewModel
+
+   /* override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUIAndNavigation(this)
+        }
+    }*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        splashViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+        splashViewModel.liveData.observe(this, {
+            when (it) {
+                is SplashState.MainActivity -> {
+                    goToMainActivity()
+                }
+            }
+        })
+    }
+
+    private fun goToMainActivity() {
+        finish()
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+
+    /*private fun hideSystemUIAndNavigation(activity: Activity) {
+        val decorView: View = activity.window.decorView
+        decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }*/
+}
